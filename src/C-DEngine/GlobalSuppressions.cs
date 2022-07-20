0ebc40fdae91ec880eaff3d936f91776e2b2f7f0
Chris Muench <chris.muench@live.com>
2022-07-20 13:25:19 -0700

    update for latest newton and missing license

--- src/C-DEngine/GlobalSuppressions.cs
+++ src/C-DEngine/GlobalSuppressions.cs
@@ -1,4 +1,7 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+﻿// SPDX-FileCopyrightText: Copyright (c) 2009-2022 TRUMPF Laser GmbH, authors: C-Labs
+//
+// SPDX-License-Identifier: MPL-2.0
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
