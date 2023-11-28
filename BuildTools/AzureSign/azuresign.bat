839f5a6d0ddcde18a749baf82703524c44edc9c4
Markus Horstmann <markushorstmann@hotmail.com>
2023-11-28 09:53:09 -0800

    Azure Sign tooling

---
+++ BuildTools/AzureSign/azuresign.bat
@@ -0,0 +1,2 @@
+echo Signing with Azure Vault Key
+%~dp0\AzureSignTool.exe sign -du "https://c-labs.com" -fd sha1 -kvu %CDE_KVU% -kvi %CDE_KVI% -kvt %CDE_KVT% -kvs %CDE_KVS% -kvc %CDE_KVC% -tr http://timestamp.digicert.com -td sha1 %1
