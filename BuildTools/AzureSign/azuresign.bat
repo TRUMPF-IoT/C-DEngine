4d2af68c58d107bc345f747b9d53bcc2bc1b94c5
Markus Horstmann <markushorstmann@hotmail.com>
2023-11-29 10:41:13 -0800

    ReUse License info

--- BuildTools/AzureSign/azuresign.bat
+++ BuildTools/AzureSign/azuresign.bat
@@ -1,2 +1,4 @@
+rem SPDX-FileCopyrightText: Copyright (c) 2009-2020 TRUMPF Laser GmbH, authors: C-Labs
+rem SPDX-License-Identifier: CC0-1.0
 echo Signing with Azure Vault Key
 AzureSignTool.exe sign -du "https://c-labs.com" -fd sha1 -kvu %CDE_KVU% -kvi %CDE_KVI% -kvt %CDE_KVT% -kvs %CDE_KVS% -kvc %CDE_KVC% -tr http://timestamp.digicert.com -td sha1 %1
