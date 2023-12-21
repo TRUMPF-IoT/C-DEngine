6dd8cd9eb6abd5e00cb68dc938ee3c4380b61995
Chris Muench <chris.muench@live.com>
2023-12-21 13:32:45 -0800

    signme fix

--- BuildTools/signMeSha.bat
+++ BuildTools/signMeSha.bat
@@ -8,5 +8,6 @@ call ""%CDE_CODE_SIGN%\azuresecrets.bat"
 echo Signing with Azure Vault Key
 AzureSignTool.exe sign -du "https://c-labs.com" -fd sha1 -kvu %CDE_KVU% -kvi %CDE_KVI% -kvt %CDE_KVT% -kvs %CDE_KVS% -kvc %CDE_KVC% -tr http://timestamp.digicert.com -td sha1 %1
 :noSign
+goto exit
 echo !!!!!!! Skipped signing: No "%~dp0\real.sign" file found - expected if not an official build.
 :exit
