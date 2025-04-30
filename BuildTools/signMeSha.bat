5503cc89ad18ea3c0f43c98105c432b4d67f314e
Chris Muench <chris@hyviva.com>
2025-04-30 15:29:34 -0700

    Different time server for code-signing

--- BuildTools/signMeSha.bat
+++ BuildTools/signMeSha.bat
@@ -6,7 +6,8 @@ echo Setting Azure Vault secrets
 call ""%CDE_CODE_SIGN%\azuresecrets.bat"
 :noSecrets
 echo Signing with Azure Vault Key
-AzureSignTool.exe sign -du "https://c-labs.com" -fd sha384 -kvu %CDE_KVU% -kvi %CDE_KVI% -kvt %CDE_KVT% -kvs %CDE_KVS% -kvc %CDE_KVC% -tr http://timestamp.digicert.com -td sha384 %1
+AzureSignTool.exe sign -du "https://c-labs.com" -fd sha384 -kvu %CDE_KVU% -kvi %CDE_KVI% -kvt %CDE_KVT% -kvs %CDE_KVS% -kvc %CDE_KVC% -tr http://rfc3161timestamp.globalsign.com/advanced -td sha384 %1
+rem AzureSignTool.exe sign -du "https://c-labs.com" -fd sha384 -kvu %CDE_KVU% -kvi %CDE_KVI% -kvt %CDE_KVT% -kvs %CDE_KVS% -kvc %CDE_KVC% -td sha384 %1
 rem AzureSignTool.exe sign -du "https://c-labs.com" -fd sha1 -kvu %CDE_KVU% -kvi %CDE_KVI% -kvt %CDE_KVT% -kvs %CDE_KVS% -kvc %CDE_KVC% -td sha1 %1
 goto exit
 :noSign
